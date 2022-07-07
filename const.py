f3a4e696a448a7bb7194e7eb54365a46e8975ba1
netr0m <moamu@equinor.com>
2022-07-07 12:46:18 +0200

    constants

---
+++ const.py
@@ -0,0 +1,7 @@
+AZ_RBAC_PIM_BASE_URL = "https://api.azrbac.mspim.azure.com"
+AZ_RBAC_PIM_BASE_PATH = "api/v2/privilegedAccess"
+AZ_AUTHORITY = f"https://login.microsoftonline.com/"
+AZ_PIM_SCOPE = "01fc33a7-78ba-4d2f-a4b7-768e336e890e/.default"
+
+DEFAULT_REASON = "config"
+DEFAULT_DURATION_MINUTES = 480
